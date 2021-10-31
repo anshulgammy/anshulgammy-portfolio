a7ed5215012361e31c2e9d7ea3ee6444e4081d5d
Anshul Gautam <anshulgammy@gmail.com>
2021-10-31 21:35:18 +0530

    Initial Commit!

---
+++ components/Header/HeaderText.tsx
@@ -0,0 +1,67 @@
+import { Box, Typography } from "@material-ui/core";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import Buttons from "./Buttons";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+      alignContent: "center",
+      justifyContent: "center",
+      flexDirection: "column",
+      [theme.breakpoints.down("md")]: {
+        marginTop: theme.spacing(6),
+      },
+    },
+    title: {
+      paddingRight: "20%",
+    },
+    hi: {
+      color: "red",
+    },
+    paragraph: {
+      paddingRight: "20%",
+    },
+  })
+);
+
+function HeaderText() {
+  const classes = useStyles();
+  return (
+    <Box
+      component="main"
+      role="main"
+      itemScope
+      itemType="https://schema.org/Person"
+      className={classes.root}
+    >
+      <meta itemProp="familyName" content="Tomar" />
+      <meta itemProp="gender" content="https://schema.org/Male" />
+      <meta itemProp="name" content="Anshul Gautam" />
+      <Typography
+        className={classes.title}
+        variant="h2"
+        component="h1"
+        gutterBottom
+      >
+        <span className={classes.hi}>hi! 👋
+          <br /></span>I am{" "}
+        <span itemProp="givenName">Anshul.</span>
+      </Typography>
+      <Typography
+        variant="h4"
+        itemProp="disambiguatingDescription"
+        component="h2"
+        gutterBottom
+      >
+        I am a Software Engineer and Blogger from India
+      </Typography>
+      <Typography itemProp="knowsAbout" className={classes.paragraph}>
+        With a detail oriented focus, I enjoy creating simple but effective software solutions, improve application performance with ease of maintenance, and enhance the overall user experience.
+      </Typography>
+      <Buttons />
+    </Box>
+  );
+}
+
+export default HeaderText;
---
+++ lib/logos.ts
@@ -0,0 +1,190 @@
+type logoNames =
+  | "apollo"
+  | "graphql"
+  | "nestjs"
+  | "react"
+  | "bootstrap"
+  | "chartjs"
+  | "expressjs"
+  | "git"
+  | "github"
+  | "materialui"
+  | "mongodb"
+  | "nextjs"
+  | "s3"
+  | "socketio"
+  | "typescript"
+  | "vercel"
+  | "ejs"
+  | "nodejs"
+  | "javascript"
+  | "postgresql"
+  | "redux"
+  | "sass"
+  | "bash"
+  | "reactSpring"
+  | "framerMotion"
+  | "java"
+  | "spring"
+  | "cassandra"
+  | "docker"
+  | "aws"
+  | "azure"
+  | "heroku"
+  | "mssql";
+
+type Logos = {
+  [k in logoNames]: {
+    label: string;
+    logo: string;
+    spin?: boolean;
+    invert?: boolean;
+  };
+};
+
+const logos: Logos = {
+  apollo: {
+    label: "Apollo",
+    logo: "/assets/icons/apollo.svg",
+    spin: false,
+    invert: true,
+  },
+  graphql: {
+    label: "GraphQl",
+    logo: "/assets/icons/graphql.svg",
+    spin: false,
+  },
+  nestjs: {
+    label: "Nest.js",
+    logo: "/assets/icons/nestjs.svg",
+    spin: false,
+  },
+  react: {
+    label: "React",
+    logo: "/assets/icons/react.png",
+    spin: true,
+  },
+  bootstrap: {
+    label: "Bootstrap",
+    logo: "/assets/icons/bootstrap.svg",
+  },
+  chartjs: {
+    label: "Chart.js",
+    logo: "/assets/icons/chartjs.svg",
+  },
+  expressjs: {
+    label: "Express.js",
+    logo: "/assets/icons/expressjs.png",
+  },
+  git: {
+    label: "Git",
+    logo: "/assets/icons/git.svg",
+  },
+  github: {
+    label: "GitHub",
+    logo: "/assets/icons/github.svg",
+    invert: true,
+  },
+  materialui: {
+    label: "Material-UI",
+    logo: "/assets/icons/materialui.svg",
+  },
+  mongodb: {
+    label: "MongoDB",
+    logo: "/assets/icons/mongodb.svg",
+  },
+  nextjs: {
+    label: "Next.js",
+    logo: "/assets/icons/nextjs.svg",
+    invert: true,
+  },
+  s3: {
+    label: "AWS-S3",
+    logo: "/assets/icons/s3.svg",
+  },
+  socketio: {
+    label: "Socket.IO",
+    logo: "/assets/icons/socketio.svg",
+  },
+  typescript: {
+    label: "TypeScript",
+    logo: "/assets/icons/typescript.svg",
+  },
+  vercel: {
+    label: "Vercel",
+    logo: "/assets/icons/vercel.svg",
+    invert: true,
+  },
+  ejs: {
+    label: "EJS",
+    logo: "/assets/icons/ejs.svg",
+    invert: true,
+  },
+  nodejs: {
+    label: "Node.js",
+    logo: "/assets/icons/nodejs.svg",
+  },
+  javascript: {
+    label: "JavaScript",
+    logo: "/assets/icons/javascript.svg",
+  },
+  postgresql: {
+    label: "PostgreSQL",
+    logo: "/assets/icons/postgresql.svg",
+  },
+  redux: {
+    label: "Redux",
+    logo: "/assets/icons/redux.svg",
+  },
+  sass: {
+    label: "Sass",
+    logo: "/assets/icons/sass.svg",
+  },
+  bash: {
+    label: "Bash",
+    logo: "/assets/icons/bash.svg",
+  },
+  reactSpring: {
+    label: "react-spring",
+    logo: "/assets/icons/react-spring.svg",
+  },
+  framerMotion: {
+    label: "Framer Motion",
+    logo: "/assets/icons/framer.png",
+  },
+  java: {
+    label: "Java",
+    logo: "/assets/icons/java.svg"
+  },
+  spring: {
+    label: "Spring Boot",
+    logo: "/assets/icons/spring.svg"
+  },
+  cassandra: {
+    label: "Cassandra",
+    logo: "/assets/icons/cassandra.svg",
+    invert: true
+  },
+  docker: {
+    label: "Docker",
+    logo: "/assets/icons/docker.svg",
+  },
+  aws: {
+    label: "AWS",
+    logo: "/assets/icons/aws.svg",
+  },
+  azure: {
+    label: "Azure",
+    logo: "/assets/icons/azure.svg",
+  },
+  heroku: {
+    label: "Heroku",
+    logo: "/assets/icons/heroku.svg",
+  },
+  mssql: {
+    label: "MS SQL Server",
+    logo: "/assets/icons/mssql.svg",
+  }
+};
+
+export default logos;
